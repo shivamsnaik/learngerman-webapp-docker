571b735342fd6877950721204477403cb4afb4ce
Shivam Naik <ichbinshivamsnaik@outlook.com>
2024-06-09 19:55:35 +0200

    Update new word page: Updated logic to add new nouns verbs and adjectives

--- pages/new-words.tsx
+++ pages/new-words.tsx
@@ -3,21 +3,42 @@ import styles from "../styles/NewWords.module.scss"
 import { useEffect, useRef, useState } from "react"
 import { app, database } from '../firebaseConfig';
 import { collection, addDoc, getDocs, CollectionReference, setDoc, doc } from 'firebase/firestore';
-import {getAuth, signInWithEmailAndPassword, onAuthStateChanged, browserSessionPersistence} from 'firebase/auth';
-import {Spinner} from 'react-bootstrap'
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, browserSessionPersistence } from 'firebase/auth';
+import { Spinner } from 'react-bootstrap'
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+
+const wordTypes = [
+  {
+    value: "noun",
+    label: "Noun"
+  },
+  {
+    value: "verb",
+    label: "Verb"
+  },
+  {
+    value: "adjective",
+    label: "Adjective"
+  }
+];
+
 export default function NewWords() {
 
-  const [pageInitialized, setPageInitStatus] = useState(false)
-  const [dbInstance, setDbInstance] = useState(collection(database, "german-word-meanings"));
+  const [pageInitialized, setPageInitStatus] = useState(false);
+  const [isPasswordCorrect, setPasswordCorrect] = useState(true);
   const username = useRef(null);
   const password = useRef(null);
-  const word = useRef(null);
-  const meaning = useRef(null);
+  const [word, setWord] = useState(null)
+  const [meaning, setMeaning] = useState(null)
+  const [wordType, setWordType] = useState(null)
+
   const addWordResponse = useRef(null);
 
   const [userSignedIn, setUserSigninStatus] = useState(false);
-  
+
 
   useEffect(() => {
 
@@ -25,12 +46,12 @@ export default function NewWords() {
     const auth = getAuth();
     auth.setPersistence(browserSessionPersistence)
     onAuthStateChanged(auth, (user) => {
-      if(user){
+      if (user) {
         //User is logged in
         console.log("User is logged in")
         setUserSigninStatus(true)
       }
-      else{
+      else {
         //User logged out
         console.log("User is not logged in")
       }
@@ -41,81 +62,117 @@ export default function NewWords() {
 
     });
 
-  },[userSignedIn]);
+  }, [userSignedIn]);
 
   // Authenticate the user on Firebase
-  const authenticateUser = (e:any) => {
+  const authenticateUser = (e: any) => {
     e.preventDefault();
     const auth = getAuth()
     signInWithEmailAndPassword(auth, username.current.value, password.current.value)
       .then((userCredential) => {
         setUserSigninStatus(true)
+        // hides the wrong password if it is being displayed 
+        !isPasswordCorrect && setPasswordCorrect(true)
       })
       .catch((error) => {
         console.log(error.code, error.message)
+        setPasswordCorrect(false)
       });
   };
 
   // Add the entered word and its meaning to the firestore database
-  const  addWordToList = (event:any) => {
+  const addWordToList = (event: any) => {
     event.preventDefault();
-    addDoc(dbInstance, {
-      word: word.current.value.toLowerCase(),
-      meaning: meaning.current.value.toLowerCase()
+
+    let collectionName = null;
+    if(wordType === "noun") {collectionName = "german-words-nouns"}
+    else if(wordType === "verb") {collectionName = "german-words-verbs"}
+    else {collectionName = "german-words-adjectives"}
+    
+    addDoc(collection(database, collectionName), {
+      word: word,
+      meaning: meaning
     })
-      .then(()=> {
+      .then(() => {
 
       })
       .catch(error => {
         console.log("ERROR: ", error.code, error.message)
       });
-
   };
 
 
-    return (
+  return (
 
-              <Page style={{marginTop: "5rem"}}>
-                {!pageInitialized
-                ?
-                  <div style={{display: "flex", justifyContent: "center"}}>
-                    <Spinner style={{justifyContent: "center"}} animation="border" size="sm" />
-                  </div>
-                :
-                  <>
-                    { userSignedIn 
-                    ?
-                    <>
-                      <p className={styles.headline}>Enter the word and its meaning</p>
-                      <div className={styles.form_content}>
-                        <div className={styles.word_wrapper}>
-                          <input ref={word} key={"word"} type="text" name="word" required={true} placeholder="Word" autoComplete="off"/>
-                        </div>
-                        <div className={styles.meaning_wrapper}>
-                          <input ref={meaning} key={"meaning"}  type="text" name="meaning" required={true} placeholder="Meaning" autoComplete="off"/>
-                        </div>
-                        <Button onClick={addWordToList}>Submit</Button>
-                        <p ref={addWordResponse}></p>
-                      </div>
-                    </>
-                    : 
-                    <>
-                      <p className={styles.headline}>Enter your credentials</p>
-                      <div className={styles.form_content}>
-                        <div className={styles.username_wrapper}>
-                          <input ref={username} key={"username"}  type="text" name="username" required={true} placeholder="Username"/>
-                        </div>
-                        <div className={styles.password_wrapper}>
-                          <input ref={password} key={"password"}  type="password" name="password" required={true} placeholder="Password"/>
-                        </div>
-                        <Button onClick={authenticateUser}>Submit</Button> 
-                      </div>
-                    </>
-                    }
+    <Page style={{ marginTop: "5rem" }}>
+      {!pageInitialized
+        ?
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Spinner style={{ justifyContent: "center" }} animation="border" size="sm" />
+        </div>
+        :
+        <>
+          {userSignedIn
+            ?
+            <div className={styles.pageWrapper}>
+              <p className={styles.headline}>Enter the word and its meaning</p>
+              <div className={styles.form_content}>
+                <TextField
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setWord(event.target.value);
+                  }}
+                  required id="word-description" label="Word" variant="outlined" />
+                <TextField
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setMeaning(event.target.value);
+                  }}
+                  required id="word-meaning" label="Meaning" variant="outlined" />
+                <TextField
+                  value={wordType}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setWordType(event.target.value);
+                  }}
+                  required
+                  id="filled-select-currency"
+                  select
+                  label="Select"
+                  defaultValue="EUR"
+                  helperText="Please select the word type"
+                  variant="filled"
                   
-                  </>
+                >
+                  {wordTypes.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <Button variant="contained" onClick={addWordToList}>Submit</Button>
+                <p ref={addWordResponse}></p>
+              </div>
+            </div>
+            :
+            <>
+              <p className={styles.headline}>Enter your credentials</p>
+              <div className={styles.form_content}>
+                <div className={styles.username_wrapper}>
+                  <input ref={username} key={"username"} type="text" name="username" required={true} placeholder="Username" />
+                </div>
+                <div className={styles.password_wrapper}>
+                  <input ref={password} key={"password"} type="password" name="password" required={true} placeholder="Password" />
+                </div>
+                {!isPasswordCorrect && <div className={styles.wrong_password_wrapper}>
+                  <p>Wrong Username/Password</p>
+                </div>
                 }
-                
-              </Page>
-    );
+                <Button onClick={authenticateUser}>Submit</Button>
+              </div>
+            </>
+          }
+
+        </>
+      }
+
+    </Page>
+  );
 };
