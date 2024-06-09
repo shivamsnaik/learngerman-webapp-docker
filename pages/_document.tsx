5454d3dab69503ee40c4d3aaaa867978e762b97f
Shivam Naik <ichbinshivamsnaik@outlook.com>
2024-06-09 15:09:19 +0200

    Update _document.tsx: Replace crossOrigin property from true to anonymous; Update next.config.js to remove unsupported output object

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>  
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>  
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"/> 
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" 
